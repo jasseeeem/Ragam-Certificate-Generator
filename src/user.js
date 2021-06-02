47b25f23c2ac5be9145ead0d84ae663febfd12c0
Jaseem001 <jaseemseems@gmail.com>
2021-06-02 19:28:39 +0300

    Added category as a parameter

--- src/user.js
+++ src/user.js
@@ -1,5 +1,6 @@
-const base = "/Ragam-Certificate-Generator"
+// const base = "/Ragam-Certificate-Generator"
 // const base =""
+const base = "/certificates"
 
 MD5 = function(e) {
     function h(a, b) {
